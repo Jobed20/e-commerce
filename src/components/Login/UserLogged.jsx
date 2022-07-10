12f719939fa4e113e112f6f9f072946a99b82d46
Obed <obedestrada52@gmail.com>
2022-07-10 01:32:54 -0600

    ecommerce

--- src/components/Login/UserLogged.jsx
+++ src/components/Login/UserLogged.jsx
@@ -4,8 +4,8 @@ import './login.css'
 const UserLogged = () => {
   return (
     <article className='loggedUser'>
-      <i className="fa-solid fa-user-check"></i>
-      <h2 className='logged'>You are alredy Logged</h2>
+     
+      <h2 className='logged'> <i className="fa-solid fa-user-check"></i>You are alredy Logged</h2>
     </article>
   )
 }
